f6ae5e628a3449343b778d44fdac3caac4a40dca
Muhammad Bin Kamran <muhammadbkamran@gmail.com>
2020-08-07 23:25:35 +0500

    AJAX 1.0.0

---
+++ AJAX_Library/ajax-lib-1.0.js
@@ -0,0 +1,36 @@
+(function(global) {
+	let ajaxMain = {}
+
+	let verifyAjax = function() {
+		if (XMLHttpRequest) {
+			return (new XMLHttpRequest);
+		} else {
+			global.alert("AJAX not supported in your current browser");
+		};
+	};
+
+	let makeRequest = function(requestURL, responseHandler, JSONresource) {  //to be used by the programmer
+		let userRequest = verifyAjax();
+		userRequest.onreadystatechange = function() {
+			return verifyConnection(userRequest, responseHandler, JSONresource);
+		};
+		userRequest.open("GET", requestURL, true);
+		userRequest.send(null);
+	}
+
+	let verifyConnection = function(userRequest, responseHandler, JSONresource) {
+		if ((userRequest.readyState === 4) && (userRequest.status === 200)) {
+			responseHandler = function() {
+				if ((JSONresource) || (JSONresource === undefined)) {
+					return (JSON.parse(request.responseText));
+				} else {
+					return (request.responseText);
+				};	
+			};
+			
+		};
+	};
+
+	global.$ajaxMain = ajaxMain;
+
+})(window);
